71eef1456c6b02b7cfbbeeb71bad0d6373a75536
xudong <xuyadong@abupdate.com>
2023-03-27 20:43:18 +0800

    feat:问诊卡片

---
+++ src/views/Room/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import RoomStatus from './components/RoomStatus.vue'
+import RoomAction from './components/RoomAction.vue'
+import RoomMessage from './components/RoomMessage.vue'
+</script>
+
+<template>
+  <div class="room-page">
+    <cp-nav-bar title="问诊室" />
+    <room-status />
+    <room-message></room-message>
+    <room-action></room-action>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
