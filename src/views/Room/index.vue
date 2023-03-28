ec9ffcd6184d4f14d9fbf55c5131c414180cc908
xudong <xuyadong@abupdate.com>
2023-03-28 21:57:43 +0800

    feat:socket添加

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -2,6 +2,41 @@
 import RoomStatus from './components/RoomStatus.vue'
 import RoomAction from './components/RoomAction.vue'
 import RoomMessage from './components/RoomMessage.vue'
+
+import { io } from 'socket.io-client'
+import type { Socket } from 'socket.io-client'
+import { onMounted, onUnmounted } from 'vue'
+import { baseURL } from '@/utils/request'
+import { useUserStore } from '@/stores'
+import { useRoute } from 'vue-router'
+const store = useUserStore()
+const route = useRoute()
+let socket: Socket
+onUnmounted(() => {
+  socket.close()
+})
+onMounted(() => {
+  io(baseURL, {
+    auth: {
+      token: `Bearer ${store.user?.token}`
+    },
+    query: {
+      orderId: route.query.orderId
+    }
+  })
+  socket.on('connect', () => {
+    console.log('打印成功')
+  })
+  socket.on('error', (event) => {
+    // 错误异常消息
+    console.log('error')
+  })
+
+  socket.on('disconnect', () => {
+    // 已经断开连接
+    console.log('disconnect')
+  })
+})
 </script>
 
 <template>
